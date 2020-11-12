ab9d192fd65f6e02b435a409974d4a12ac7a175e
Denis Rodionov <nicholas.hawkwood@gmail.com>
2020-11-12 23:44:46 +0200

    Sample DB query and API exposure

---
+++ Api/Controllers/BrandController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using Timweb.Models;
+
+namespace Timweb.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BrandController : ControllerBase
+    {
+        private readonly ILogger<BrandController> _logger;
+        private readonly IConfiguration _configuration;
+        
+        public BrandController(ILogger<BrandController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Brand>> Get()
+        {
+            await using (var conn = new NpgsqlConnection(_configuration.GetConnectionString("Postgres"))) 
+            {
+                await conn.OpenAsync();
+                return conn.Query<Brand>("select * from brands;");
+            }
+        }
+    }
+}
--- Models/Brand.cs
+++ Models/Brand.cs
@@ -2,9 +2,11 @@
 
 namespace Timweb.Models
 {
-    public class Brand
+    public record Brand
     {
+        public int Id { get; }
         public string Logo { get; set; }
-        public Name Name { get; set; }
+        public string NameRu { get; set; }
+        public string NameEn { get; set; }
     }
 }
