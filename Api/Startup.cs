9606ad425067110ff23363b9405862fc2e3fdfc0
Denis Rodionov <nicholas.hawkwood@gmail.com>
2020-11-14 15:08:51 +0200

    Wired up comments for Swagger

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,7 +24,12 @@ namespace Timweb.Api
         {
             services.AddControllers();
             services.AddSingleton<IDatabase, Database>();
-            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Api", Version = "v1"}); });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Api", Version = "v1"});
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Api.xml"));
+                c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, "Models.xml"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
