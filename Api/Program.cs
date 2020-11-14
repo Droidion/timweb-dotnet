4a52a0c418b7cd9c253adb4073aa54943d6e4d30
Denis Rodionov <nicholas.hawkwood@gmail.com>
2020-11-14 16:42:54 +0200

    Added Sentry for logging

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Sentry.Extensibility;
 
 namespace Timweb.Api
 {
@@ -15,7 +16,16 @@ namespace Timweb.Api
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseSentry(o =>
+                    {
+                        o.Debug = true;
+                        o.MaxRequestBodySize = RequestSize.Always;
+                        o.Dsn = "https://eaea80984b194cffaff91bbd0d7d21b7@o476686.ingest.sentry.io/5516699";
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
         }
     }
 }
