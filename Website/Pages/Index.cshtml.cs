3713d12790b6715e9f07639aa7231ba223c33c4c
Denis Rodionov <nicholas.hawkwood@gmail.com>
2020-11-30 01:14:50 +0200

    Extracted api query logic

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -1,59 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Net.Http;
-using System.Text;
-using System.Text.Json;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using Timweb.Models;
+using Timweb.Website.Services;
 
 namespace Timweb.Website.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly IConfiguration _configuration;
+        private readonly IRequestMaker _requestMaker;
+
         public IEnumerable<Brand> Brands { get; private set; } = new List<Brand>();
 
-        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory clientFactory, IConfiguration configuration)
+        public IndexModel(ILogger<IndexModel> logger, IRequestMaker requestMaker)
         {
             _logger = logger;
-            _clientFactory = clientFactory;
-            _configuration = configuration;
+            _requestMaker = requestMaker;
         }
 
         public async Task OnGet()
         {
-            var urlBase = _configuration["ApiUrl"];
-            var request = new HttpRequestMessage(HttpMethod.Get, urlBase + "brand");
-            request.Headers.Add("Accept", "application/json");
-            request.Headers.Add("User-Agent", "HttpClientFactory");
-            request.Headers.Add("Authorization", "Bearer " + GenerateJwtToken());
-
-            var client = _clientFactory.CreateClient();
-
-            var response = await client.SendAsync(request);
-            
-            if (response.IsSuccessStatusCode)
-            {
-                await using var responseStream = await response.Content.ReadAsStreamAsync();
-                Brands = await JsonSerializer.DeserializeAsync<IEnumerable<Brand>>(responseStream) ?? new List<Brand>();
-            }
-        }
-        
-        private string GenerateJwtToken()
-        {
-            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]));
-            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
-            var token = new JwtSecurityToken(signingCredentials: credentials);
-            var encoded = new JwtSecurityTokenHandler().WriteToken(token);
-            Console.WriteLine(encoded);
-            return encoded;
+            Brands = await _requestMaker.Get<Brand>("brand");
         }
     }
 }
--- Website/Startup.cs
+++ Website/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Timweb.Website.Services;
 
 namespace Timweb.Website
 {
@@ -20,6 +21,7 @@ namespace Timweb.Website
         {
             services.AddHttpClient();
             services.AddRazorPages();
+            services.AddSingleton<IRequestMaker, RequestMaker>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
